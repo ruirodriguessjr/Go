698ab73fdd51ca9e824bb8df6cb43518807629f2
Rui Rodrigues <rui.rsjr@gmail.com>
2019-07-10 18:29:17 -0300

    Alterando pastas, colocando cada .go dentro de sua pasta respectiva

---
+++ restapi/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"math/rand"
+	"strconv"
+	"GitHub\Go\src\github.com\gorilla\mux"
+)
+
+func main(){
+	fmt.Println("Hello world")
+}
