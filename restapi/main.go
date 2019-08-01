2c501bb68a5fc59b6d6a961d9e014ca2c2fd1960
Rui Rodrigues <rui.rsjr@gmail.com>
2019-07-31 21:11:01 -0300

    Atualizando API MUX

--- restapi/main.go
+++ restapi/main.go
@@ -1,22 +1,22 @@
 package main
 
 import (
-	"encoding/json"
 	"log"
 	"net/http"
-	"math/rand"
-	"strconv"
-	"github.com\gorilla\mux"
+
+	"github.com/gorilla/mux"
 )
 
 // Author Struct
 type Author struct {
-	Fistname string `json:"firstname"`
-	Lastname string `json:"lastname"`
+	Firstname string `json:"firstname"`
+	Lastname  string `json:"lastname"`
 }
 
-// Book Struct (Model)
+// Init Books var as a slice Book struct
+var books []Book
 
+// Book Struct (Model)
 type Book struct {
 	ID     string  `json:"id"`
 	Isbn   string  `json:"isbn"`
@@ -31,27 +31,31 @@ func getBooks(w http.ResponseWriter, r *http.Request) {
 
 // Get single Book
 func getBook(w http.ResponseWriter, r *http.Request) {
-	
+
 }
 
 // Create a New Book
 func createBook(w http.ResponseWriter, r *http.Request) {
-	
+
 }
 
 //Update Book
 func updateBook(w http.ResponseWriter, r *http.Request) {
-	
+
 }
 
 // Delete Book
 func deleteBook(w http.ResponseWriter, r *http.Request) {
-	
+
 }
 
 func main() {
 	// Init Route
 	r := mux.NewRouter()
+
+	// Mock Data - @todo - implement DB
+	books = append(books, Book{ID: "1", Isbn: "448743", Title: "Book One", Author: &Author{Firstname: "John", Lastname: "Doe"}})
+	books = append(books, Book{ID: "2", Isbn: "847564", Title: "Book Two", Author: &Author{Firstname: "Steve", Lastname: "Smith"}})
 
 	// Route Handlers / Endpoints
 	r.HandleFunc("/api/books", getBooks).Methods("GET")
