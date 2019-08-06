6ce37ed92837bfae54e0b49700aed027b4d0ecc4
Rui Rodrigues <rsantosjunio@dxc.com>
2019-08-06 14:05:07 -0300

    Atualizações da api

--- restapi/main.go
+++ restapi/main.go
@@ -1,9 +1,10 @@
 package main
 
 import (
-	// O pacote json implementa a codificação e a decodificação de JSON
-	"log"      //O log de pacotes implementa um pacote de log simples. Ele define um tipo, Logger, com métodos para formatação de saída.
-	"net/http" // O pacote http fornece implementações de cliente e servidor HTTP. Get, Head, Post e PostForm fazem solicitações HTTP (ou HTTPS)
+	"encoding/json" // O pacote json implementa a codificação e a decodificação de JSON
+	"log"           //O log de pacotes implementa um pacote de log simples. Ele define um tipo, Logger, com métodos para formatação de saída.
+	"net/http"      // O pacote http fornece implementações de cliente e servidor HTTP. Get, Head, Post e PostForm fazem solicitações HTTP (ou HTTPS)
+
 	// Pacote aritmético/ rand gera número aleatório
 	// O pacote strconv implementa conversões de x para representações de strings de tipos de dados básicos.
 
@@ -12,8 +13,8 @@ import (
 
 // Author Struct
 type Author struct {
-	Fistname string `json:"firstname"`
-	Lastname string `json:"lastname"`
+	Firstname string `json:"firstname"`
+	Lastname  string `json:"lastname"`
 }
 
 // Book Struct (Model)
@@ -24,13 +25,27 @@ type Book struct {
 	Author *Author `json:"author"`
 }
 
+// Init books var as a slice Book Struct
+var books []Book
+
 // Get All Books
 func getBooks(w http.ResponseWriter, r *http.Request) {
-
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(books)
 }
 
 // Get single Book
 func getBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r) // Get Params
+	// Loop through books and find with id
+	for _, item := range books {
+		if item.ID == params["id"] {
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode(&Book{})
 
 }
 
@@ -52,6 +67,10 @@ func deleteBook(w http.ResponseWriter, r *http.Request) {
 func main() {
 	// Init Route
 	r := mux.NewRouter()
+
+	// Mock Data - @todo - implement DB
+	books = append(books, Book{ID: "1", Isbn: "447539", Title: "Book One", Author: &Author{Firstname: "John", Lastname: "Doe"}})
+	books = append(books, Book{ID: "2", Isbn: "843921", Title: "Book Two", Author: &Author{Firstname: "Steve", Lastname: "Smith"}})
 
 	// Route Handlers / Endpoints
 	r.HandleFunc("/api/books", getBooks).Methods("GET")
